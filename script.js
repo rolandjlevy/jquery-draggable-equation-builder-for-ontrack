1ea7578a596ebc48c8239bffd45b6835faec017f
Roland Levy <rolandjlevy@gmail.com>
2020-12-09 17:02:49 +0000

    Adding comments to new class

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
 $(function() {
 
-  /**************/
-  /* Initialise */
-  /**************/
+  /********************/
+  /* Define constants */
+  /********************/
 
-  // var uiElementWidth = 125;
-
-  // Define sortable menu items
-  var menus = {
+  // Sortable items
+  var MENUS = {
     logical: '<select name="logical" class="ui-selectmenu-menu ui-widget ui-corner-all"><option value="AND">AND</option><option value="OR">OR</option></select>',
     bracket: '<select name="bracket" id="bracket-clone" class="ui-selectmenu-menu ui-widget ui-corner-all"><option value="(">(</option><option value=")">)</option></select>',
     comparison: '<select name="comparison" class="ui-selectmenu-menu ui-widget ui-corner-all"><option value="==">==</option><option value="!=">!=</option><option value="<">&lt;</option><option value=">">&gt;</option><option value="<=">&lt;=</option><option value=">=">&gt;=</option></select>'
   }
 
-  // Define different elements for Answers
-  var answers = [
+  // Answer types
+  var ANSWERS = [
     { type:'select', value:'<select name="boolean" class="answer ui-selectmenu-menu ui-widget ui-corner-all"><option value="yes">Yes</option><option value="no">No</option></select>' },
     { type:'select', value:'<select name="range-abc" class="answer ui-selectmenu-menu ui-widget ui-corner-all"><option value="na">N/A</option><option value="pass">Pass</option></select>' },
     { type:'select', value:'<select name="range-numeric" class="answer ui-selectmenu-menu ui-widget ui-corner-all"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option></select>' },
     { type:'input', value:'<input class="answer ui-widget ui-widget-content ui-corner-all" placeholder="Enter..." />' }
   ];
 
+  // Component class contructor
   function Component(type) {
     this.type = type;
-    this.sortableItem = $(menus[type]);
     this.menuSelector = $('select#' + type);
     this.buttonSelector = $('#add-' + type);
     this.uiElementWidth = 125;
   };
 
+  // Initialise select menus
   Component.prototype.initSelectMenu = function() {
     var component = this;
     this.menuSelector.selectmenu({
@@ -45,11 +44,12 @@ $(function() {
     });
   }
 
+  // Create Answer element in dependence upon Question menu selection
   Component.prototype.createAnswerElement = function(selectedIndex) {
     var component = this;
     var answerContainer = $('#answer-container');
     answerContainer.empty();
-    var answer = answers[selectedIndex-1];
+    var answer = ANSWERS[selectedIndex-1];
     if (answer.type == 'select') {
       $(answer.value).clone().appendTo(answerContainer).selectmenu({ 
         width: component.uiElementWidth,
@@ -64,61 +64,53 @@ $(function() {
     }
   }
 
+  // Event handler for adding a comparison, bracket and logical item
   Component.prototype.addItemEventHandler = function(width) {
     var component = this;
-    $('#add-' + component.type).unbind('click').click(function(e) {
-      var val = $('#' + component.type).val();
-      var item = component.createListItem(val);
+    this.buttonSelector.unbind('click').click(function(e) {
+      var val = component.menuSelector.val();
+      var item = component.createSortableItem(val);
       item.appendTo($('.sortable'));
       var itemContent = item.find('.item-content');
-      $(menus[component.type]).clone().appendTo($(itemContent)).selectmenu({
+      $(MENUS[component.type]).clone().appendTo($(itemContent)).selectmenu({
         width: width,
-        change: function(event, ui) {
-          $(this).parent().parent().attr('data-item', component.type + ':' + ui.item.value);
-          updateData('.sortable');
-        },
         create: function(event, ui) {
           $(this).val(val);
           $(this).selectmenu('refresh');
+        },
+        change: function(event, ui) {
+          $(this).parent().parent().attr('data-item', component.type + ':' + ui.item.value);
+          updateData('.sortable');
         }
       });
-      $('.sortable .remove').unbind('click').click(function(e) {
-        $(this).parent().remove();
-        updateData('.sortable');
-      });
-      updateData('.sortable');
-      $('.sortable').sortable('refresh');
+      component.removeEventHandler();
     });
   }
 
+  // Event handler for adding a Question item
   Component.prototype.addQuestionItemEventHandler = function() {
     var component = this;
     $('#add-question').unbind('click').click(function(){
       var selectedText = $('#question').val();
       if (!selectedText) return;
-      var item = component.createListItem(selectedText);
+      var item = component.createSortableItem(selectedText);
       var itemContent = item.find('.item-content');
       itemContent.html('<span class="item">' + selectedText + '</span>');
       item.appendTo($('.sortable'));
-      $(".sortable .remove").unbind('click').click(function(e){
-        var data = $(e.target.parentNode).attr('data-item');
-        $(this).parent().remove();
-        updateData(".sortable");
-      });
-      $(".sortable").sortable("refresh");
-      updateData(".sortable");
+      component.removeEventHandler();
     });
   }
-   
+  
+  // Event handler for adding an Answer item
   Component.prototype.addAnswerItemEventHandler = function(width) {
     var component = this;
     $('#add-answer').unbind('click').click(function(){
       var val = $('#answer-container > .answer').val();
       var selectedIndex = $('#question option:selected').index();
-      var item = component.createListItem(val);
+      var item = component.createSortableItem(val);
       item.appendTo($('.sortable'));
       var itemContent = item.find('.item-content');
-      var answer = answers[selectedIndex-1];
+      var answer = ANSWERS[selectedIndex-1];
       if (answer.type == 'select') {
         $(answer.value).clone().appendTo($(itemContent)).selectmenu({ 
           width: width,
@@ -148,11 +140,24 @@ $(function() {
       $('.sortable').sortable('refresh');
     });
   }
-    
-  Component.prototype.createListItem = function(val) {
+  
+  // Create a sortable item for sortable area
+  Component.prototype.createSortableItem = function(val) {
     var cleanedValue = val.replace(/</g, "&lt;").replace(/>/g, "&gt;");
     return $("<li data-item=" + this.type + ':' + cleanedValue + "><span class='draggable'></span><span class='item-content'></span><span class='remove'></span></li>");
   }
+
+  // Event handler for removing an item
+  Component.prototype.removeEventHandler = function() {
+    $(".sortable .remove").unbind('click').click(function(e){
+      $(this).parent().remove();
+      updateData(".sortable");
+    });
+    $(".sortable").sortable("refresh");
+    updateData(".sortable");
+  }
+
+  // Initialise components and their select menus & event handlers
 
   var questionComponent = new Component('question');
   questionComponent.initSelectMenu();
@@ -173,7 +178,7 @@ $(function() {
   comparisonComponent.initSelectMenu();
   comparisonComponent.addItemEventHandler(50);
 
-  // Initialise all select menus
+
   // $("select").selectmenu({
   //   width: uiElementWidth,
   //   change: function(e, ui) {
@@ -185,7 +190,6 @@ $(function() {
   /* Questions menu & button */
   /***************************/
 
-  // Initialise Questions select menu
   // $("select#question").selectmenu({
   //   width: uiElementWidth,
   //   change: function(e, data) {
@@ -197,12 +201,11 @@ $(function() {
   //   }
   // });
 
-  // Event handler for adding a Question item
   // function addQuestionItemEventHandler() {
   //   $('#add-question').unbind('click').click(function(){
   //     var selectedText = $('#question').val();
   //     if (!selectedText) return;
-  //     var item = createListItem('question', selectedText);
+  //     var item = createSortableItem('question', selectedText);
   //     var itemContent = item.find('.item-content');
   //     itemContent.html('<span class="item">' + selectedText + '</span>');
   //     item.appendTo($('.sortable'));
@@ -219,7 +222,6 @@ $(function() {
   /* Answers elements */
   /********************/
 
-  // Create Answer in dependence upon selection from Question menu 
   // function createAnswerElement(selectedIndex) {
   //   var answerContainer = $('#answer-container');
   //   answerContainer.empty();
@@ -238,12 +240,11 @@ $(function() {
   //   }
   // }
   
-  // Event handler for adding an answer element
   // function addAnswerItemEventHandler(width) {
   //   $('#add-answer').unbind('click').click(function(){
   //     var val = $('#answer-container > .answer').val();
   //     var selectedIndex = $('#question option:selected').index();
-  //     var item = createListItem(val);
+  //     var item = createSortableItem(val);
   //     item.appendTo($('.sortable'));
   //     var itemContent = item.find('.item-content');
   //     var answer = answers[selectedIndex-1];
@@ -277,11 +278,12 @@ $(function() {
   //   });
   // }
 
-  // Event handler for adding a comparison, bracket and logical menu
+  
+
   // function addItemEventHandler(type, width) {
   //   $('#add-' + type).unbind('click').click(function(e) {
   //     var val = $('#' + type).val();
-  //     var item = createListItem(type, val);
+  //     var item = createSortableItem(type, val);
   //     item.appendTo($('.sortable'));
   //     var itemContent = item.find('.item-content');
   //     $(menus[type]).clone().appendTo($(itemContent)).selectmenu({
@@ -304,8 +306,9 @@ $(function() {
   //   });
   // }
 
-  // Create a list item for sortable area
-  // function createListItem(type, val) {
+  
+
+  // function createSortableItem(type, val) {
   //   var cleanedValue = val.replace(/</g, "&lt;").replace(/>/g, "&gt;");
   //   return $("<li data-item=" + type + ':' + cleanedValue + "><span class='draggable'></span><span class='item-content'></span><span class='remove'></span></li>");
   // }
